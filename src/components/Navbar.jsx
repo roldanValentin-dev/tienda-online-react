df679379071d18e0c9d22a4e4db4777eaa0ec167
Valentin <valentindev10@gmail.com>
2026-04-09 23:13:24 -0300

    fix: cerrar menu mobile automaticamente al navegar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
-import { Link } from 'react-router-dom';
-import { useContext } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
 import { CarritoContext } from '../context/CarritoContext';
 
 function Navbar() {
     const { cart } = useContext(CarritoContext);
+    const location = useLocation();
     
     const totalItems = cart.reduce((sum, item) => sum + item.cantidad, 0);
+
+    // Cerrar el menú cuando cambia la ruta
+    useEffect(() => {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        const navbarCollapse = document.getElementById('navbarNav');
+        
+        // Si el menú está abierto, cerrarlo
+        if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+            navbarToggler?.click();
+        }
+    }, [location]);
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Mi Tienda</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <button 
+                    className="navbar-toggler" 
+                    type="button" 
+                    data-bs-toggle="collapse" 
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
@@ -33,5 +53,4 @@ function Navbar() {
         </nav>
     );
 }
-
 export default Navbar;
